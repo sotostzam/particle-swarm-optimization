39c9f961ba2a8a999628da0178e15429789ab136
Sotiris Tzamaras <sotos@tzamaras.com>
2020-05-03 21:40:22 +0300

    fix PSO algorithm to correctly minimize cost function
    Changes included in this commit:
    - Fix a bug while checking velocity value
    - Fix some bugs while referencing values
    - Add arrow vectors representing the velocities
    - Rename objective function
    - Implement dimensions while initializing velocities

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ class Particle():
         self.pos = [x, y]
         self.pos_z = z
         self.velocity = velocity
-        self.best_pos = self.pos
+        self.best_pos = [x, y]
 
 class Swarm():
     def __init__(self, pop, v_max):
         self.particles = []             # List of particles in the swarm
         self.best_pos = None            # Best particle of the swarm
-        self.best_pos_z = math.inf          # Best particle of the swarm
+        self.best_pos_z = math.inf      # Best particle of the swarm
 
         for _ in range(pop):
             x = np.random.uniform(-3, 3)
             y = np.random.uniform(-3, 3)
-            z = ackley(x, y)
+            z = cost_function(x, y)
             velocity = np.random.rand(2) * v_max
             particle = Particle(x, y, z, velocity)
             self.particles.append(particle)
@@ -31,8 +31,8 @@ class Swarm():
                 self.best_pos = particle.pos
                 self.best_pos_z = particle.pos_z
 
-# Evaluate Ackley function
-def ackley(x, y, a=20, b=0.2, c=2*math.pi):
+# Evaluate objective/cost function (Ackley)
+def cost_function(x, y, a=20, b=0.2, c=2*math.pi):
     term_1 = np.exp((-b * np.sqrt(0.5 * (x ** 2 + y ** 2))))
     term_2 = np.exp((np.cos(c * x) + np.cos(c * y)) / 2)
     return -1 * a * term_1 - term_2 + a + np.exp(1)
@@ -58,13 +58,14 @@ def main():
 
     curr_iter = 0
     while curr_iter < max_iterations and abs(swarm.best_pos_z - GLOBAL_BEST) > CONVERGENCE:
-        r1 = np.random.uniform(0, 1)
-        r2 = np.random.uniform(0, 1)
-
         for particle in swarm.particles:
-            # Update particle's velocity
-            particle.velocity[0] += (PERSONAL_C * r1 * (particle.best_pos[0] - particle.pos[0])) + (SOCIAL_C * r2 * (swarm.best_pos[0] - particle.pos[0]))
-            particle.velocity[1] += (PERSONAL_C * r1 * (particle.best_pos[1] - particle.pos[1])) + (SOCIAL_C * r2 * (swarm.best_pos[1] - particle.pos[1]))
+
+            for i in range(0, dimensions):
+                r1 = np.random.uniform(0, 1)
+                r2 = np.random.uniform(0, 1)
+                
+                # Update particle's velocity
+                particle.velocity[i] += PERSONAL_C * r1 * (particle.best_pos[i] - particle.pos[i]) + SOCIAL_C * r2 * (swarm.best_pos[i] - particle.pos[i])
 
             # Check if velocity is exceeded
             if particle.velocity[0] > v_max:
@@ -73,43 +74,46 @@ def main():
                 particle.velocity[0] = -v_max
             if particle.velocity[1] > v_max:
                 particle.velocity[1] = v_max
-            if particle.velocity[1] > -v_max:
+            if particle.velocity[1] < -v_max:
                 particle.velocity[1] = -v_max
 
-            # Update particle's position
+            # Update particle's current position
             particle.pos[0] += particle.velocity[0]
             particle.pos[1] += particle.velocity[1]
-            particle.pos_z = ackley(particle.pos[0], particle.pos[1])
+            particle.pos_z = cost_function(particle.pos[0], particle.pos[1])
 
             # Update swarm's best known position
             if particle.pos_z < swarm.best_pos_z:
-                swarm.best_pos = particle.pos
+                swarm.best_pos = [particle.pos[0], particle.pos[1]]   # FIXME Why do i need to do this instead of particle.pos?
                 swarm.best_pos_z = particle.pos_z
-                print("New best swarm position: " + str(swarm.best_pos_z))
 
             # Update particle's best known position
-            if ackley(particle.pos[0], particle.pos[1]) < swarm.best_pos_z:
-                #print("New best position")
+            if particle.pos_z < cost_function(particle.best_pos[0], particle.best_pos[1]):
                 particle.best_pos = particle.pos
 
             # Check if particle is within boundaries
-            if particle.pos[0] > 3 or particle.pos[1] > 3:
+            if particle.pos[0] > 3:
                 particle.pos[0] = np.random.uniform(-3, 3)
+                particle.pos_z = cost_function(particle.pos[0], particle.pos[1])
+            if particle.pos[1] > 3:
                 particle.pos[1] = np.random.uniform(-3, 3)
-                particle.pos_z = ackley(particle.pos[0], particle.pos[1])
-            if particle.pos[0] < -3 or particle.pos[1] < -3:
+                particle.pos_z = cost_function(particle.pos[0], particle.pos[1])
+            if particle.pos[0] < -3:
                 particle.pos[0] = np.random.uniform(-3, 3)
+                particle.pos_z = cost_function(particle.pos[0], particle.pos[1])
+            if particle.pos[1] < -3:
                 particle.pos[1] = np.random.uniform(-3, 3)
-                particle.pos_z = ackley(particle.pos[0], particle.pos[1])
+                particle.pos_z = cost_function(particle.pos[0], particle.pos[1])
 
         plt.clf()
         ax = fig.add_subplot(1, 2, 1, projection='3d')
         axc = fig.add_subplot(1, 2, 2)
-        ax.plot_surface(X, Y, ackley(X, Y), cmap='coolwarm')
-        axc.contourf(X, Y, ackley(X, Y))
+        ax.plot_surface(X, Y, cost_function(X, Y), cmap='coolwarm')
+        axc.contourf(X, Y, cost_function(X, Y))
         for particle in swarm.particles:
-            ax.scatter(particle.pos[0], particle.pos[1], particle.pos_z, marker='*', c='r')
-            axc.scatter(particle.pos[0], particle.pos[1], marker='*', c='r')  
+            ax.scatter(particle.pos[0], particle.pos[1], particle.pos_z, marker='*', c='g')
+            axc.scatter(particle.pos[0], particle.pos[1], marker='*', c='r')
+            axc.arrow(particle.pos[0], particle.pos[1], particle.velocity[0], particle.velocity[1], head_width=0.05, head_length=0.1, fc='k', ec='k')
         plt.pause(0.001)
 
         curr_iter += 1
